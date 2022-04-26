2191c43f61704ee0745a6f3531c0db56ad7c96dc
Denis <denisstadniczuk@gmail.com>
2022-04-26 14:18:14 +0200

    Add build step back

--- build.py
+++ build.py
@@ -11,8 +11,8 @@ import glob
 
 pathlib.Path('build/out').mkdir(parents=True, exist_ok=True)
 
-# print('Building rehax core')
-# os.system('cd out && make rehax-cpp-mac && cp rehax-cpp-mac/bin/Debug/librehax-cpp-mac.a ../build/out/librehax-mac.a')
+print('Building rehax core')
+os.system('cd out && make rehax-cpp-mac && cp rehax-cpp-mac/bin/Debug/librehax-cpp-mac.a ../build/out/librehax-mac.a')
 
 header_files = [[path, 'build/out/include/' + path] for path in glob.glob('rehax/components/**/**/*.h')]
 
